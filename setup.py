b38e7e7d75c8bd376cdaa18360ce94f37a6c984a
Cody Lee <codylee@wellaware.us>
2014-05-02 12:12:10 -0500

    Version Bump - Reconnect and Exception Handling

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='rexster,tinkerpop,rexpro,graphdb',
-    install_requires=['msgpack-python', 'six==1.6.1', 'gevent>=1.0'],
+    install_requires=['msgpack-python>=0.4.0', 'six==1.6.1', 'gevent>=1.0'],
     extra_require={
         'develop': ['nose==1.3.0', 'coverage==3.7.1', 'tox==1.7.1', 'celery==3.1.11', 'redis==2.9.1', 'tox>=1.7.1',
                     'detox>=0.9.3'],
@@ -41,9 +41,8 @@ setup(
     author_email='bdeggleston@gmail.com',
     maintainer='Cody Lee',
     maintainer_email='codylee@wellaware.us',
-    url='https://github.com/bdeggleston/rexpro-python',
+    url='https://github.com/platinummonkey/rexpro-python',
     license='BSD',
     packages=find_packages(),
     include_package_data=True,
 )
-
